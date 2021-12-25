d86b002e1db6d01e19cc3066233196e4df1b19a5
object1037 <contact@object1037.dev>
2021-12-26 00:14:59 +0900

    get class datas

--- pages/api/get-classes.ts
+++ pages/api/get-classes.ts
@@ -7,25 +7,15 @@ const conn = new PSDB('main')
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const session = await getSession({ req })
   const {
-    body: {
-      class_time,
-      class_title,
-      class_url,
-    },
     method
   } = req
+  if (!session) {
+    return res.status(200)
+  }
   switch (method) {
-    case 'POST':
-      const [rows, fields] = await conn.query(`
-      insert into accounts (class_time, class_title, class_url, account_id)
-      values ('${class_time}', '${class_title}', '${class_url}', '${session?.account.account_id}')
-      `, '')
-      res.status(201).json({})
-      break
     case 'GET':
       try {
-        const [getRows, _] = await conn.query('select * from account', '')
-        console.log(getRows)
+        const [getRows, _] = await conn.query(`select * from class where account_id='${session?.user?.email}'`, '')
         res.status(200).json(getRows)
       } catch (e) {
         res.status(500).json({ message: 'An error occurred while connecting to the database' })
@@ -33,7 +23,6 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
       break
     default:
-      res.setHeader('Allow', ['GET', 'PUT'])
       res.status(405).end(`Method ${method} Not Allowed`)
   }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,34 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import ClassCard from '../components/classCard'
-import { classes } from '../test'
+import { useSession } from 'next-auth/react'
 import axios from 'axios'
 import { signIn, signOut } from "next-auth/react"
+import useSWR from 'swr'
+
+const fetcher = (url: string) => axios.get(url).then(res => res.data)
 
 const Home: NextPage = () => {
+  const {data: session, status} = useSession()
+  const { data, error } = useSWR('/api/get-classes', fetcher)
+  console.log(data)
   async function clickHandler() {
     await axios.get('/api/account', {
     })
+  }
+  if (session) {
+    return (
+      <>
+      <Head>
+        <title>oclist</title>
+        <meta name="description" content="online class list" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <main>
+        <p>logged in</p>
+      </main>
+      </>
+    )
   }
   return (
     <>
