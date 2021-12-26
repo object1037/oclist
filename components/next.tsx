0ef05c4ab8c3ef4b9b5e08c3fcc90c93daa32df1
object1037 <contact@object1037.dev>
2021-12-27 05:47:13 +0900

    memorize nextTime

--- components/next.tsx
+++ components/next.tsx
@@ -1,7 +1,7 @@
 import ClassCard from "./classCard"
 import axios from "axios"
 import useSWR from "swr"
-import { useEffect, useState } from "react"
+import { useEffect, useState, useMemo } from "react"
 
 const fetcher = (url: string) => axios.get(url).then(res => res.data)
 
@@ -11,7 +11,6 @@ const Next = ({
   data: classData[] | undefined
 }) => {
   const [time, setTime] = useState(0)
-  let next = -1
 
   useEffect(() => {
     const now = new Date()
@@ -22,44 +21,49 @@ const Next = ({
   }, [])
 
   const { data: account, error } = useSWR('/api/get-settings', fetcher)
+
+  let nextTime = useMemo(() => {
+    if (!account) {
+      return -1
+    }
+
+    const settings = [
+      account[0].range_0,
+      account[0].range_1,
+      account[0].range_2,
+      account[0].range_3,
+      account[0].range_4,
+      account[0].range_5,
+    ]
+  
+    let times = new Array()
+
+    for (let i = 0; i < 6; i++) {
+      times.push(new Array())
+    }  
+
+    for (let i = 0; i < 6; i++) {
+      for (let j = 0; j < 6; j++) {
+        times[i].push({
+          start: settings[j].substring(0, 5),
+          end: settings[j].substring(6),
+        })
+        const startTime = 24 * 60 * i + +settings[j].substring(0, 2) * 60 + +settings[j].substring(3, 5)
+        if (time < startTime) {
+          return (i * 6 + j)
+        }
+      }
+    }
+
+    return 0
+  }, [account])
+
   if (!data || !account) {
     return <></>
   }
 
-  const settings = [
-    account[0].range_0,
-    account[0].range_1,
-    account[0].range_2,
-    account[0].range_3,
-    account[0].range_4,
-    account[0].range_5,
-  ]
-
-  let times = new Array()
-  let flag = false
-
-  for (let i = 0; i < 6; i++) {
-    times.push(new Array())
-  }
-
-  for (let i = 0; i < 6; i++) {
-    for (let j = 0; j < 6; j++) {
-      times[i].push({
-        start: settings[j].substring(0, 5),
-        end: settings[j].substring(6),
-      })
-      const startTime = 24 * 60 * i + +settings[j].substring(0, 2) * 60 + +settings[j].substring(3, 5)
-      console.log(startTime)
-      if (time < startTime && !flag) {
-        next = (i * 6 + j)
-        flag = true
-        break;
-      }
-    }
-  }
-
   return (
-    <ClassCard classData={data[next]} class_time={next}/>
+    <ClassCard classData={data[nextTime]} class_time={nextTime}/>
   )
 }
 
