a57986c4ac639d2e3e5b67d07d72a50034e4a68b
object1037 <contact@object1037.dev>
2021-12-29 20:53:30 +0900

    fix bad query

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import { PSDB } from 'planetscale-node'
+import SQL from 'sql-template-strings'
 
 const conn = new PSDB('main')
 
@@ -17,10 +18,11 @@ export default NextAuth({
   events: {
     async signIn(message) {
       const account = message.user
-      const [rows, fields] = await conn.query(`
+      const query = SQL`
       insert ignore into account (account_id, account_email, range_0, range_1, range_2, range_3, range_4, range_5)
-      values ('${account.id}', '${account.email}', '08:30', '10:25', '13:15', '15:10', '17:05', '19:00')
-      `, '')
+      values (${account.id}, ${account.email}, '08:30', '10:25', '13:15', '15:10', '17:05', '19:00')
+      `
+      const [rows, fields] = await conn.query(query.sql, query.values)
     },
   },
   pages: {
